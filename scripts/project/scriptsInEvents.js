4b8fb059642dc3a9aca30d11b2400a521659dfc0
Shadou0419 <109211053+Shadou0419@users.noreply.github.com>
2023-12-10 23:14:29 +0800

    1210up
    add

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,29 @@
+
+
+
+const scriptsInEvents = {
+
+	async 中耳聽小骨_Event8_Act6(runtime, localVars)
+	{
+		
+	},
+
+	async 中耳聲音傳導2事件表_Event5_Act4(runtime, localVars)
+	{
+		document.getElementById("HTML元素").style.display = "none";
+	},
+
+	async 中耳聲音傳導2事件表_Event6_Act4(runtime, localVars)
+	{
+		document.getElementById("HTML元素").style.display = "none";
+	},
+
+	async 中耳聲音傳導2事件表_Event7_Act2(runtime, localVars)
+	{
+		document.getElementById("HTML元素").style.display = "block";
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
